2f97ace7b71ac42fdab11af0688d151c1ad88862
yaseminege29-ai <yaseminege29@gmail.com>
2025-12-26 17:10:50 +0300

    feat: add analysis script and movie dataset

---
+++ github 2. proje(pandas .1)/analysis.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+df= pd.read_csv("movies.csv")
+print("\n ---ilk 5 satır---")
+print(df.head())
+
+print("\n---bilgi---")
+print(df.info())
+
+print("\n---istatistikler---")
+print(df.describe())
+
+print("\n---ortalama puan---")
+ortalama = df["rating"].mean()
+print(ortalama)
+
+print("\n---8.0 ve üzeri filmler---")
+high_rated = df[df["rating"]>=8.0]
+[["title","rating"]]
+print(high_rated)
+
+print("\n---Türlere göre ortalama puan---")
+gendre_avg = df.groupby("gendre")["rating"].mean()
+print(gendre_avg)
+
+print("\n---en yüksek puanlı film---")
+best = df.loc[df["rating"].idxmax(),["title","rating"]]
+print(best)
+
+print("\n---yıllara göre ortalama puan---")
+year_avg=df.groupby("year")["rating"].mean()
+print(year_avg)
